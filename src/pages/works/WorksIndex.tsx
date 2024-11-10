44a778e7c4e765f8c06a51a3d69b9742988db2f7
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-11-10 11:51:10 +0330

    chore:add new projects data

--- src/pages/works/WorksIndex.tsx
+++ src/pages/works/WorksIndex.tsx
@@ -3,7 +3,7 @@ import data from "../../data/portfolio.json";
 export default function WorksIndex(props: { isDark: boolean }) {
   return (
     <div className="w-screen min-h-screen pb-10">
-      <section className="w-full grid gap-5 md:gap-10 md:grid-cols-2 lg:grid-cols-3 pt-24 px-5 md:px-20 justify-items-center">
+      <section className="w-full grid justify-items-center gap-5 md:gap-10 md:grid-cols-2 lg:grid-cols-3 pt-24 px-5 md:px-20">
         {data.map(
           (
             item: {
@@ -16,8 +16,8 @@ export default function WorksIndex(props: { isDark: boolean }) {
             index: number
           ) => {
             return (
-              <a href={item.link} target="_blank">
-                <div key={index} className="move-in-down">
+              <a href={item.link} key={index} target="_blank">
+                <div className="move-in-down">
                   <div className="overflow-hidden rounded-lg dark:shadow-lg">
                     <img
                       className="hover:scale-105 hover:-rotate-1 transition-all duration-300"
