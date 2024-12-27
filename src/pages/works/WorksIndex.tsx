cc5f76ccab4bc45ee3806186935e1b82d9b3053e
Saleh Moosavi <salehmoosavi77@gmail.com>
2024-12-27 15:33:28 +0330

    feat:add techs to every item of Portfolio

--- src/pages/works/WorksIndex.tsx
+++ src/pages/works/WorksIndex.tsx
@@ -1,39 +1,43 @@
 import data from "../../data/portfolio.json";
 
+type itemType = {
+  id: number;
+  title: string;
+  desc: string;
+  techs: string[];
+  isFavor: boolean;
+  img: string;
+  imgDark: string;
+  link: string;
+};
+
 export default function WorksIndex(props: { isDark: boolean }) {
   return (
     <div className="w-screen min-h-screen pb-10">
       <section className="w-full grid justify-items-center gap-5 md:gap-10 md:grid-cols-2 lg:grid-cols-3 pt-24 px-5 md:px-20">
-        {data.map(
-          (
-            item: {
-              img: string;
-              title: string;
-              desc: string;
-              imgDark: string;
-              link: string;
-            },
-            index: number
-          ) => {
-            return (
-              <a href={item.link} key={index} target="_blank">
-                <div className="move-in-down">
-                  <div className="overflow-hidden rounded-lg dark:shadow-lg">
-                    <img
-                      className="hover:scale-105 hover:-rotate-1 transition-all duration-300"
-                      src={props.isDark ? item.img : item.imgDark}
-                      alt=""
-                    />
-                  </div>
-                  <h2 className="text-center pt-3 font-semibold">
-                    {item.title}
-                  </h2>
-                  <p className="text-center text-sm">{item.desc}</p>
+        {data.map((item: itemType) => {
+          return (
+            <a href={item.link} key={item.id} target="_blank">
+              <div className="move-in-down">
+                <div className="overflow-hidden rounded-lg dark:shadow-lg">
+                  <img
+                    className="hover:scale-105 hover:-rotate-1 transition-all duration-300"
+                    src={props.isDark ? item.img : item.imgDark}
+                    alt=""
+                  />
                 </div>
-              </a>
-            );
-          }
-        )}
+                <h2 className="text-center pt-3 font-semibold">{item.title}</h2>
+                <p className="text-center text-sm">{item.desc}</p>
+                <ul className="text-xs text-justify mt-2 font-semibold flex flex-wrap justify-center gap-x-3">
+                  <li>Techs : </li>
+                  {item.techs.map((tech: string, index: number) => {
+                    return <li key={index}>{tech}</li>;
+                  })}
+                </ul>
+              </div>
+            </a>
+          );
+        })}
       </section>
     </div>
   );
