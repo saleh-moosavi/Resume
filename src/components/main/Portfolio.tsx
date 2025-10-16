69dfb130d6153e232d91e866255a0e97cc7b6162
Saleh Moosavi <salehmoosavi77@gmail.com>
2025-10-16 16:48:40 +0330

    refactor: replace PortfolioItem with Slider component and update portfolio data structure
    - Replace the PortfolioItem component with a new Slider component for improved functionality and design.
    - Update portfolio.json to change 'link' to 'demoLink' and add 'codeLink' for better clarity and access to project resources.
    - Adjust the portfolioType interface to reflect the new data structure, enhancing type safety and maintainability.

--- src/components/main/Portfolio.tsx
+++ src/components/main/Portfolio.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import PortfolioItem from "./PortfolioItem";
+import PortfolioItem from "./Slider";
 
 export default function Portfolio() {
   return (
--- src/types/dataType.ts
+++ src/types/dataType.ts
@@ -8,7 +8,8 @@ export interface portfolioType {
   isFavor: boolean;
   img: string;
   imgDark: string;
-  link: string;
+  demoLink: string;
+  codeLink: string;
 }
 
 export interface infoType {
