73fb4b88f1bf1672e0181b780f55f37ab9304354
Saleh Moosavi <salehmoosavi77@gmail.com>
2025-11-09 11:41:42 +0330

    refactor: enhance NavBarSide component and update custom styles
    - Add a backdrop section to the NavBarSide component for improved visibility when the sidebar is open.
    - Replace the close button with an icon from react-icons for better aesthetics.
    - Update CSS styles for smoother animations and transitions, including adjustments to scrollbar styles and animation durations.

--- src/components/NavBarSide.tsx
+++ src/components/NavBarSide.tsx
@@ -1,45 +1,55 @@
 import { Link } from "react-router-dom";
 import { NavBarSideType } from "../types/dataType";
-
-
+import { BiX } from "react-icons/bi";
 
 export default function NavBarSide({ isClose, handleSideBar }: NavBarSideType) {
   return (
     <div>
+      <section
+        onClick={handleSideBar}
+        className={`bg-my-white/20 fixed inset-0 z-[500] backdrop-blur-sm ${
+          isClose ? "invisible" : "visible"
+        }`}
+      ></section>
       <ul
-        className={`flex flex-col gap-y-5 fixed top-0 bottom-0 right-0 w-2/3 p-10 hover:*:cursor-pointer bg-gray-800 dark:bg-gray-300 shadow-lg z-[1000] font-semibold *:transition-all *:duration-300 transition-all duration-300 ${
+        className={`flex flex-col gap-y-5 fixed top-0 bottom-0 right-0 w-2/3 p-10 hover:*:cursor-pointer bg-my-lightgray dark:bg-my-darkgray shadow-lg z-[1000] font-semibold transition-all duration-300 ${
           isClose ? "translate-x-full" : "translate-x-0"
         }`}
       >
-        <li
-          onClick={handleSideBar}
-          className="text-orange-500 hover:scale-125 hover:text-rose-600 dark:hover:text-rose-600 w-fit"
-        >
-          X
+        <li onClick={handleSideBar}>
+          <BiX className="size-8 text-orange-500 hover:rotate-180 hover:text-rose-600 dark:hover:text-rose-600 transition-all duration-300" />
         </li>
-        <li
-          className="dark:*:text-black hover:*:text-stone-300 dark:hover:*:text-stone-500 mt-10"
-          onClick={handleSideBar}
-        >
-          <Link to="works">WORKS</Link>
+        <li className="mt-5" onClick={handleSideBar}>
+          <Link
+            className="text-my-black hover:opacity-50 dark:text-my-white transition-all duration-300"
+            to="works"
+          >
+            WORKS
+          </Link>
         </li>
-        <li
-          className="dark:*:text-black hover:*:text-stone-300 dark:hover:*:text-stone-500"
-          onClick={handleSideBar}
-        >
-          <Link to="blog">BLOG</Link>
+        <li onClick={handleSideBar}>
+          <Link
+            className="text-my-black hover:opacity-50 dark:text-my-white transition-all duration-300"
+            to="blog"
+          >
+            BLOG
+          </Link>
         </li>
-        <li
-          className="dark:*:text-black hover:*:text-stone-300 dark:hover:*:text-stone-500"
-          onClick={handleSideBar}
-        >
-          <Link to="about">ABOUT</Link>
+        <li onClick={handleSideBar}>
+          <Link
+            className="text-my-black hover:opacity-50 dark:text-my-white transition-all duration-300"
+            to="about"
+          >
+            ABOUT
+          </Link>
         </li>
-        <li
-          className="dark:*:text-black hover:*:text-stone-300 dark:hover:*:text-stone-500"
-          onClick={handleSideBar}
-        >
-          <Link to="contact">CONTACT</Link>
+        <li onClick={handleSideBar}>
+          <Link
+            className="text-my-black hover:opacity-50 dark:text-my-white transition-all duration-300"
+            to="contact"
+          >
+            CONTACT
+          </Link>
         </li>
       </ul>
     </div>
